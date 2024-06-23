f56dbcc5b64819bdc81f763d2eab92fe57b023d7
vbx1404 <vbot@cogeco.net>
2024-06-22 17:56:40 -0700

    change for berkeley hack

--- Chatbot.py
+++ Chatbot.py
@@ -1,5 +1,6 @@
 from openai import OpenAI
 import streamlit as st
+from streamlit_lottie import st_lottie
 
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
@@ -7,8 +8,8 @@ with st.sidebar:
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-st.title("💬 Chatbot")
-st.caption("🚀 A Streamlit chatbot powered by OpenAI")
+st.title("💬🌱 Chat With Me!")
+st.caption("🚀 A Streamlit chatbot powered by Groq and Intel")
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
@@ -23,7 +24,7 @@ if prompt := st.chat_input():
     client = OpenAI(api_key=openai_api_key)
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+    response = client.chat.completions.create(model="gpt-4", messages=st.session_state.messages)
     msg = response.choices[0].message.content
     st.session_state.messages.append({"role": "assistant", "content": msg})
     st.chat_message("assistant").write(msg)
--- pages/4_Langchain_PromptTemplate.py
+++ pages/4_Langchain_PromptTemplate.py
@@ -9,7 +9,7 @@ openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
 
 def blog_outline(topic):
     # Instantiate LLM model
-    llm = OpenAI(model_name="text-davinci-003", openai_api_key=openai_api_key)
+    llm = OpenAI(model_name="gpt-4", openai_api_key=openai_api_key)
     # Prompt
     template = "As an experienced data scientist and technical writer, generate an outline for a blog about {topic}."
     prompt = PromptTemplate(input_variables=["topic"], template=template)
